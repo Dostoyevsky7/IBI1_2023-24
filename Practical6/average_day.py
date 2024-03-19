9270e8a4f2fc4901acb18897b4bfccea544062c7
Dostoyevesky7 <yufei.23@intl.zju.edu.cn>
2024-03-19 20:57:34 +0800

    Initialise

---
+++ Practical6/average_day.py
@@ -0,0 +1,16 @@
+t_other = 24-8-6-3.5-2-1 #calculate the 'other' activity time
+dict_time = {'Sleeping':8, 'Classes':6, 'Studying':3.5, 'TV':2, 'Music':1, 'Other':t_other} #create a dictionary
+print(dict_time) #print out dictionary
+print(dict_time['Sleeping']) #the part 'Sleeping' can change depends on the user
+#print the amount of time of a certain activity on the list
+import numpy as np
+import matplotlib.pyplot as plt
+label = ['Sleeping', 'Classes', 'Studying', 'TV', 'Music', 'Other']
+time = [8,6,3.5,2,1,t_other]
+plt.figure()
+plt.pie(time, labels=label, startangle=90) #draw a pie chart
+plt.title("the average day of a university student") #add a title to this figure
+plt.show()
+plt.clf()
+a_time = dict_time['Sleeping'] #the part 'Sleeping' can change depends on the user
+print(a_time) #print the amount of time of a certain activity on the list
---
+++ Practical6/city_size.py
@@ -0,0 +1,31 @@
+uk_cities = [0.56, 0.62, 0.04, 9.7]
+chn_cities = [0.58, 8.4, 29.9, 22.2]
+print(uk_cities)
+print(chn_cities)
+#print out 2 lists
+import numpy as np
+import matplotlib.pyplot as plt
+#import 2 librabries
+
+#the bar chart of UK
+ind1 = ['Edinburgh', 'Glasgow', 'Stirling', 'London'] #the indexes are the names of the cities
+width1 = 0.5
+plt.figure()
+plt.bar(ind1, uk_cities, width1, color = 'cornflowerblue') # choose a blue color
+plt.ylabel("population(millions)")
+plt.title("population of different cities in UK") #add a chart title
+plt.xticks(ind1)
+plt.show()
+plt.clf()
+
+#the bar chart for China
+ind2 = ['Haining', 'Hangzhou', 'Shanghai', 'Beijing']
+width2 = 0.3
+color_option = ['brown', 'firebrick', 'indianred', 'lightcoral'] #every bar can have a different color
+plt.figure()
+plt.bar(ind2, chn_cities, width2, color = color_option) #change the width to 0.3
+plt.ylabel("population(millions)")
+plt.title("population of different cities in China")
+plt.xticks(ind2)
+plt.show()
+plt.clf() #remember to type clf
