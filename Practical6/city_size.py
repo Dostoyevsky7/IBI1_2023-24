8cadc7b80f676e3cb17f5407ecc997636c5dfd24
Dostoyevesky7 <yufei.23@intl.zju.edu.cn>
2024-03-20 15:31:53 +0800

    Initialise

--- Practical6/city_size.py
+++ Practical6/city_size.py
@@ -1,5 +1,7 @@
 uk_cities = [0.56, 0.62, 0.04, 9.7]
+uk_cities.sort()
 chn_cities = [0.58, 8.4, 29.9, 22.2]
+chn_cities.sort()
 print(uk_cities)
 print(chn_cities)
 #print out 2 lists
@@ -8,7 +10,7 @@ import matplotlib.pyplot as plt
 #import 2 librabries
 
 #the bar chart of UK
-ind1 = ['Edinburgh', 'Glasgow', 'Stirling', 'London'] #the indexes are the names of the cities
+ind1 = ['Stirling', 'Edinburgh', 'Glasgow', 'London'] #the indexes are the names of the cities
 width1 = 0.5
 plt.figure()
 plt.bar(ind1, uk_cities, width1, color = 'cornflowerblue') # choose a blue color
@@ -19,7 +21,7 @@ plt.show()
 plt.clf()
 
 #the bar chart for China
-ind2 = ['Haining', 'Hangzhou', 'Shanghai', 'Beijing']
+ind2 = ['Haining', 'Hangzhou', 'Beijing', 'Shanghai']
 width2 = 0.3
 color_option = ['brown', 'firebrick', 'indianred', 'lightcoral'] #every bar can have a different color
 plt.figure()
