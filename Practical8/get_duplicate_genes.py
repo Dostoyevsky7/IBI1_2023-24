24a1e666ef41f0b33fe2bb2a2af88c21d33fabe6
Dostoyevesky7 <yufei.23@intl.zju.edu.cn>
2024-04-05 16:50:03 +0800

    Initialise

--- Practical8/get_duplicate_genes.py
+++ Practical8/get_duplicate_genes.py
@@ -1,5 +1,5 @@
 import re   
-seqlist = open('duplicate_genes.fa','w')
+seqlist = open('duplicate_genes.fa','w', encoding = 'utf-8')
 genelist = open('Saccharomyces_cerevisiae.R64-1-1.cdna.all.fa','r')
 gene = genelist.read()
 #find put all the lines have the word'duplication', make it into one large string
@@ -8,9 +8,11 @@ split_dup = "".join(find_dup)
 #find all the gene names in it
 find_name = re.findall(r">([^']+?_mRNA)", split_dup)
 #the names allhave the format"'>YXXXXX", delete the single quotation "'"
-cleaned_names = [name.replace("'", "") for name in find_name]
-#write name and sequence into the file 
-for cleaned_name in cleaned_names:    
-    match = re.search(rf">({cleaned_name})_mRNA\n([^>]+)", gene)  
-    if match:  
-        seqlist.write(f">{cleaned_name}_mRNA\n{match.group(2)}\n")
+cleaned_names = [name.replace("Y", ">Y") for name in find_name]
+#write name and sequence into the file
+for cleaned_name in cleaned_names:
+    match = re.search(cleaned_name+ r'\n([^>]+)', gene)
+    print('yestoo') 
+    if match:
+        print('yes')
+        seqlist.write(cleaned_name + match.group(2) + r'\n')
