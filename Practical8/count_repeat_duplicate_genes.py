b34825b0a9ebc28d07da03f2b9061416fccd0203
Dostoyevesky7 <yufei.23@intl.zju.edu.cn>
2024-04-10 16:49:27 +0800

    Initialise

--- Practical8/count_repeat_duplicate_genes.py
+++ Practical8/count_repeat_duplicate_genes.py
@@ -3,21 +3,22 @@ import re
 rep_seq = input('Input a repetitive sequence:')
 #read and create files needed
 file_name = rep_seq + '_duplicate_genes.fa'
-output_f = open(file_name, 'w')
-input_f = open('duplicate genes.fa')
-sequence = input_f.read()
+output_f = open(r'D:/Downloadssss/IBI1_2023-24/Practical8' + '\\' + file_name, 'w')
+sequence = open('duplicate_genes.fa')
 #make every gene name and its corresponding sequence into one single string
-l_string = ''
-for line in sequence:
-    if line.startswith('>'):
-        l_string += '\n'+line+' '
-    else:
-        l_string += line
+with open('store.fa', 'w') as l_string:
+    for line in sequence:
+        if line.startswith('>'):
+            l_string.write('\n'+line[:-1])
+        else:
+            l_string.write(line[:-1])
 #search each gene if it contains target sequence
-for line in l_string:
-    if re.search(rep_seq,line):
-        time = line.count(rep_seq) #count the repetition times
-        name = re.findall(r'>.+?_mRNA', line)
-        new_name = name + time
-        new_line = re.sub(new_name, name,line)
-        output_f.write(new_line + '\n')
+with open('store.fa', 'r') as l_read:
+    for line in l_read:
+        if rep_seq in line:
+            time = line.count(rep_seq)
+            name = re.findall(r'>.+?_mRNA', line)
+            name_str = str(name)
+            new_name = re.escape(name_str) + str(time)
+            new_line = line.strip(name_str)
+            output_f.write(new_name + '\n' + new_line)
