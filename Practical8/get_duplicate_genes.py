da837ac5684177793f584e1a64def8cc55608bfc
Dostoyevesky7 <yufei.23@intl.zju.edu.cn>
2024-04-10 16:49:43 +0800

    Initialise

--- Practical8/get_duplicate_genes.py
+++ Practical8/get_duplicate_genes.py
@@ -1,18 +1,20 @@
 import re   
 seqlist = open('duplicate_genes.fa','w', encoding = 'utf-8')
-genelist = open('Saccharomyces_cerevisiae.R64-1-1.cdna.all.fa','r')
-gene = genelist.read()
-#find put all the lines have the word'duplication', make it into one large string
-find_dup = re.findall(r'.+duplication.+\n', gene)
-split_dup = "".join(find_dup)
-#find all the gene names in it
-find_name = re.findall(r">([^']+?_mRNA)", split_dup)
-#the names allhave the format"'>YXXXXX", delete the single quotation "'"
-cleaned_names = [name.replace("Y", ">Y") for name in find_name]
-#write name and sequence into the file
-for cleaned_name in cleaned_names:
-    match = re.search(cleaned_name+ r'\n([^>]+)', gene)
-    print('yestoo') 
-    if match:
-        print('yes')
-        seqlist.write(cleaned_name + match.group(2) + r'\n')
+gene = open('Saccharomyces_cerevisiae.R64-1-1.cdna.all.fa','r')
+flag = False
+clean = ''
+sequence = ''
+for line in gene:
+    if line.startswith('>'):
+        if 'duplication' in line:
+            flag = True
+            clean = line.strip()
+            gene_name = clean.split()[0][1:]
+            seqlist.write(f">{gene_name}\n")
+        else:
+            flag = False
+            clean = ''
+            sequence = ''
+    elif flag == True:
+        sequence += line.strip()
+        seqlist.write(line)
