c6a58357fa0d99eb2879ec015d68f1c183a68dac
Dostoyevesky7 <yufei.23@intl.zju.edu.cn>
2024-04-24 17:02:53 +0800

    Upload Practical10

--- Practical10/SIR_vaccination.py
+++ Practical10/SIR_vaccination.py
@@ -1,6 +1,7 @@
 #import necessary libraries
 import numpy as np
 import matplotlib.pyplot as plt
+import pandas as pd
 from matplotlib import cm
 N = 10000
 s = 9999
@@ -8,7 +9,7 @@ i = 1
 r = 0
 beta = 0.3
 gamma = 0.05
-colors = ['b','c','g','m','r','w','y','slateblue','deeppink','maroon','darkmagenta']
+colors = ['b','c','g','m','r','y','slateblue','deeppink','maroon','darkmagenta', 'sienna']
 V = [0,10,20,30,40,50,60,70,80,90,100]
 S = [s]
 I = [i]
@@ -17,10 +18,11 @@ T = [0]
 #start a figure
 plt.figure(figsize=(6,4),dpi=150)
 # for different vaccination rate, reset the value of s
-for i in range(11):
-    s = 9999 * (100-V[i]) * 0.01
+for a in range(11):
+    s = 9999 * (100-V[a]) * 0.01
     i = 1
     r = 0
+    S, I, R, T = [s], [i], [r], [0]
     for j in range(1,1000):
         #loop for 1000 days
         #infected people recover
@@ -34,8 +36,7 @@ for i in range(11):
         I.append(i)
         R.append(r)
         T.append(j)
-    plt.plot(T,I,label=str(V[i])+'%',c=colors[i])
-
+    plt.plot(T,I,label=str(V[a])+'%',color=colors[a])
 plt.legend()
 plt.show()
 plt.clf()
