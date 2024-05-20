2dfde34e72d95b1d919260921a923d549e645514
Dostoyevsky7 <156776918+Dostoyevsky7@users.noreply.github.com>
2024-05-20 18:35:25 +0800

    upload modified file for changed counting method

--- Practical8/repetitive_counter.py
+++ Practical8/repetitive_counter.py
@@ -1,5 +1,13 @@
 seq = 'ATGCAATCGGTGTGTCTGTTCTGAGAGGGCCTAA'
-rep_1 = seq.count('GTGTGT') #count how many times of GTGTGT are there
-rep_2 = seq.count('GTCTGT') #count how many times of GTCTGT are there
-print('the sequence has ' + rep_1 +'times of "GTGTGT"')
-print('the sequence has ' + rep_2 +'times of "GTCTGT"')
+#count how many times of GTGTGT are there
+rep_1 = 0
+for i in range (len(seq)):
+    if seq[i:i+6] == 'GTGTGT':
+        rep_1 += 1
+#count how many times of GTCTGT are there
+rep_2 = 0
+for j in range (len(seq)):
+    if seq[j:j+6] == 'GTCTGT':
+        rep_2 += 1 
+print('the sequence has ', rep_1, 'times of "GTGTGT"')
+print('the sequence has ', rep_2, 'times of "GTCTGT"')
