a9cd815ebefac952143ee556175efb531d2ca702
Dostoyevsky7 <156776918+Dostoyevsky7@users.noreply.github.com>
2024-05-20 11:28:54 +0800

    Delete Practical8 directory to upload a better version

--- Practical8/repetitive_counter.py
+++
@@ -1,5 +0,0 @@
-seq = 'ATGCAATCGGTGTGTCTGTTCTGAGAGGGCCTAA'
-rep_1 = seq.count('GTGTGT') #count how many times of GTGTGT are there
-rep_2 = seq.count('GTCTGT') #count how many times of GTCTGT are there
-print('the sequence has ' + rep_1 +'times of "GTGTGT"')
-print('the sequence has ' + rep_2 +'times of "GTCTGT"')
